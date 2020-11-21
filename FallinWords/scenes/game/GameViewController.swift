6f2693a565b78cc1b04cfdca53b9b950e5b98808
bakirtas <>
2020-11-22 00:13:40 +0100

    Initial commit

---
+++ FallinWords/scenes/game/GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewController.swift
+//  FallinWords
+//
+//  Created by Hasan on 22.11.20.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Falling Words"
+    }
+}
